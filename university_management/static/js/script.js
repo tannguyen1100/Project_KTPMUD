6980cc7d8b8b17631619613bcec296b92dc5e210
pphilong21 <ppphilong21>
2021-12-29 14:22:54 +0700

    Student_New

--- university_management/static/js/script.js
+++ university_management/static/js/script.js
@@ -4,3 +4,10 @@ $(function() {
                    $('#sidebar, #content').toggleClass('active');
                });
               });
+
+$(function() {
+     // Sidebar toggle behavior
+                 $('#sidebarCollapse-small').on('click', function() {
+                     $('#sidebar, #content').toggleClass('active');
+                     });
+                });
