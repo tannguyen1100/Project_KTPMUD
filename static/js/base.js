f1e63bcd4d8da0c754d45160760180ba445d62b4
pphilong21 <ppphilong21>
2022-01-24 21:46:21 +0700

    noi_dung_do_an

--- static/js/base.js
+++ static/js/base.js
@@ -11,7 +11,26 @@ $(function() {
                      $('#sidebar, #content').toggleClass('active');
                      });
                 });
-
+$(function() {
+    //btn add giảng viên 
+            $('#icon_add-teacher--project').on('click', function() {
+                const icon = $('#add_teacher--project');
+                console.log(icon);
+                const icon_1 = $('add_teacher--project-on');
+                console.log(icon_1);
+                $( '#add_teacher--project').toggleClass('add_teacher--project-on');
+            })
+})
+$(function() {
+    //btn add giảng viên 
+            $('#icon_add-student--project').on('click', function() {
+                const icon = $('#add_teacher--project');
+                console.log(icon);
+                const icon_1 = $('add_teacher--project-on');
+                console.log(icon_1);
+                $( '#add_student--project').toggleClass('add_student--project-on');
+            })
+})
 
 
 
